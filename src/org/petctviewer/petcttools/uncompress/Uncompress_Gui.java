a115bd57548099b18c4e581b5758aed6241a7358
salim <salim@salim-NUC7i5BNH>
2018-10-21 11:29:36 +0200

    uncompressor

--- src/org/petctviewer/petcttools/uncompress/Uncompress_Gui.java
+++ src/org/petctviewer/petcttools/uncompress/Uncompress_Gui.java
@@ -52,7 +52,7 @@ public class Uncompress_Gui extends JFrame implements PlugIn {
 	 * Create the frame.
 	 */
 	public Uncompress_Gui() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
