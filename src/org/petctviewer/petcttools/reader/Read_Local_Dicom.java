40054b70fb21bcbbbb600b39539cda0692d1ff2d
Salim Kanoun <salim.kanoun@gmail.com>
2019-02-14 17:31:03 +0100

    exploring new reader

--- src/org/petctviewer/petcttools/reader/Read_Local_Dicom.java
+++ src/org/petctviewer/petcttools/reader/Read_Local_Dicom.java
@@ -1,6 +1,7 @@
 package org.petctviewer.petcttools.reader;
 
 import java.io.File;
+import java.io.FilenameFilter;
 
 import ij.ImagePlus;
 import ij.io.Opener;
@@ -10,8 +11,9 @@ public class Read_Local_Dicom {
 
 	public static void main(String[] args) {
 		Read_Local_Dicom read = new Read_Local_Dicom();
-		read.readFile(new File("G:\\GAINED_Complet_CopieExportFinal\\Batch00\\11011101021001\\PET0\\1.2.840.113704.1.111.5352.1350646167.8\\CT_001_0a63112d11044b85a7d247852479b063.dcm"));
-		read.readFileBioFormat(new File("G:\\GAINED_Complet_CopieExportFinal\\Batch00\\11011101021001\\PET0\\1.2.840.113704.1.111.5352.1350646167.8\\CT_001_0a63112d11044b85a7d247852479b063.dcm"));
+		//read.readFile(new File("G:\\GAINED_Complet_CopieExportFinal\\Batch00\\11011101021001\\PET0\\1.2.840.113704.1.111.5352.1350646167.8\\CT_001_0a63112d11044b85a7d247852479b063.dcm"));
+		//read.readFileBioFormat(new File("G:\\GAINED_Complet_CopieExportFinal\\Batch00\\11011101021001\\PET0\\1.2.840.113704.1.111.5352.1350646167.8\\CT_001_0a63112d11044b85a7d247852479b063.dcm"));
+		read.recursiveScanFolder(new File("G:\\GAINED_Complet_CopieExportFinal\\Batch00\\11011101021001\\PET0"));
 	}
 	
 	public void readFile(File file) {
@@ -21,6 +23,39 @@ public class Read_Local_Dicom {
 		System.out.println(slice.getInfoProperty());
 		//opener.open("G:\\GAINED_Complet_CopieExportFinal\\Batch00\\11011101021001\\PET0\\1.2.840.113704.1.111.5352.1350646167.8\\CT_001_0a63112d11044b85a7d247852479b063.dcm");
 		//opener.openImage("G:\\GAINED_Complet_CopieExportFinal\\Batch00\\11011101021001\\PET0\\1.2.840.113704.1.111.5352.1350646167.8", "Test");
+	}
+	
+	public void recursiveScanFolder(File folder) {
+		
+		String[] directories = folder.list(new FilenameFilter() {
+			  @Override
+			  public boolean accept(File current, String name) {
+			    return new File(current, name).isDirectory();
+			  }
+		});
+		
+		File[] files = folder.listFiles(new FilenameFilter() {
+		    public boolean accept(File dir, String name) {
+		        return (name.toLowerCase().endsWith(".dcm") || !name.contains(".") );
+		    }
+		});
+		
+
+		if(directories.length==0 
+				&& files.length>0) {
+			/*for(File file : files) {
+				System.out.println(file);
+			}*/
+			
+			System.out.println(files[0]);
+			
+		}else if(directories.length>0) {
+			for(String directory:directories) {
+				recursiveScanFolder(new File(folder.toString()+File.separator+directory));
+			}
+			
+		}
+		
 	}
 	
 	public void readFileBioFormat(File file) {
