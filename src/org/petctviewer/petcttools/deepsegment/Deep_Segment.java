2c4ec0daea2d502528f00312499ed805b32a531b
Salim Kanoun <salim.kanoun@gmail.com>
2019-05-06 14:38:20 +0200

    exploring

---
+++ src/org/petctviewer/petcttools/deepsegment/Deep_Segment.java
@@ -0,0 +1,50 @@
+package org.petctviewer.petcttools.deepsegment;
+
+import java.io.IOException;
+
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+import org.deeplearning4j.nn.modelimport.keras.exceptions.InvalidKerasConfigurationException;
+import org.deeplearning4j.nn.modelimport.keras.exceptions.UnsupportedKerasConfigurationException;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.io.ClassPathResource;
+
+public class Deep_Segment {
+	
+	public static void main(String[] arg) {
+		Deep_Segment segment=new Deep_Segment();
+		try {
+			segment.test();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvalidKerasConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedKerasConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+	public void test() throws IOException, InvalidKerasConfigurationException, UnsupportedKerasConfigurationException {
+		// load the model
+		String simpleMlp = new ClassPathResource("model.h5").getFile().getPath();
+		
+		MultiLayerNetwork model = KerasModelImport.
+		                    importKerasSequentialModelAndWeights(simpleMlp);
+		
+		// make a random sample
+		INDArray features =Nd4j.create(new double[240][240][240]);
+		
+		/*for (int i=0; i<inputs; i++) 
+			
+		    features.putScalar(new int[] {i}, Math.random() < 0.5 ? 0 : 1);
+		*/
+		// get the prediction
+		//INDArray prediction = model.output(features);
+		
+	}
+}
